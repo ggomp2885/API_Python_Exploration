49eb02dffa614fe01939b357d37dd5bf6df24f8d
Greg Gompers <ggompers28@gmail.com>
2020-12-09 20:34:58 -0500

    Add - Initial methods/functions

---
+++ API_exploration.py
@@ -0,0 +1,22 @@
+### The most useful libraries/functions for API calls in Python ###
+    # Any URL can be used in a request function
+
+                            # Imports
+import requests
+
+                            # Python Functions
+# dir(r)              # shows an objects attributes and methods
+# help(r)             # more detailed view of objs attributes and methods
+
+
+                            # Methods
+# r = requests.     #saves the returned values into a variable
+# requests.get      #This
+# requests.post
+
+                            # Http Response Codes
+# 200s     #successful
+# 300s     #redirects
+# 400s     #client side failures
+# 500s     #server side failures
+
