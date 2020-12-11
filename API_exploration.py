655d33c317620bb873156c3f33fa973b44e5d9ed
Greg Gompers <ggompers28@gmail.com>
2020-12-10 22:51:13 -0500

    Add - Examples

--- API_exploration.py
+++ API_exploration.py
@@ -1,18 +1,26 @@
 ### The most useful libraries/functions for API calls in Python ###
-    # Any URL can be used in a request function
+    # Any URL can be used in a request function, does not have to be a designated "API" link
+    # Almost all APIs setup their responses in a JSON format (python dictionary)
+    # "URL Parameters" are additions to the URL, such as httpbin.org/get?page=2&count=25
+        # These can be added to the "get" method, in the form of a variable which contains this info in the format specific to the site.
 
                             # Imports
 import requests
+
+                            # Placeholder objects
+# r = requests.            #saves the returned values into a variable
 
                             # Python Functions
 # dir(r)              # shows an objects attributes and methods
 # help(r)             # more detailed view of objs attributes and methods
 
+                            # Functions
+# requests.get('URL')      #This "gets" everything available from the URL
+# requests.post('URL', params= , data= , headers= , json= ,)     # This "posts" the data to the URL, included in variables after the URL
 
                             # Methods
-# r = requests.     #saves the returned values into a variable
-# requests.get      #This
-# requests.post
+
+#r.status_code
 
                             # Http Response Codes
 # 200s     #successful
@@ -20,3 +28,24 @@ import requests
 # 400s     #client side failures
 # 500s     #server side failures
 
+
+                            # Working Example
+# import requests
+#
+# r = requests.get('https://randomfox.ca/floof')
+# fox_dict = r.json()
+# print(r.status_code)
+# print(fox_dict['image'])
+
+#dict to df
+
+                        # Working Example - Youtube Data API
+                            # looks like they may already have most of the python code in their GH.
+#import requests
+
+# api_key = 'AIzaSyD8XScoNLWmpCG_4oTwCFtqViX34UmIle0'
+
+                        # Working Example - Httpbin.org
+import requests
+r = requests.post('https://httpbin.org/post', json={'apiKey':'[aZmGv1wN41HLEE0RpPIDRZ0hAt7GUPmyjZXIgBg9]'})
+print(r.text)
